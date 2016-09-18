3a116e1207510ec78db024fe9f0cb3bee2ddcff5
Gilson Gabriel <gilson.gabriel@lais.huol.ufrn.br>
2016-09-18 14:06:01 -0300

    Calendario - Mudar ADD

---
+++ js/data.js
@@ -0,0 +1,15 @@
+var codropsEvents = {
+	'09-17-2016' : '<span> Prova de História <br> Prova de Geografia <br> Prova de PIU </span> <a href="AddHorarioEstudoData.html">+</a>',
+	'09-18-2016' : '<a href="AddHorarioEstudoData.html">+</a>',
+	'09-19-2016' : '<a href="AddHorarioEstudoData.html">+</a>',
+	'09-20-2016' : '<a href="AddHorarioEstudoData.html">+</a>',
+	'09-21-2016' : '<a href="AddHorarioEstudoData.html">+</a>',
+	'09-22-2016' : '<a href="AddHorarioEstudoData.html">+</a>',
+	'09-23-2016' : '<a href="AddHorarioEstudoData.html">+</a>',
+	'09-24-2016' : '<a href="AddHorarioEstudoData.html">+</a>',
+	'09-25-2016' : '<a href="AddHorarioEstudoData.html">+</a>',
+	'09-26-2016' : '<a href="AddHorarioEstudoData.html">+</a>',
+	'09-27-2016' : '<a href="AddHorarioEstudoData.html">+</a>',
+	'12-25-2016' : '<span>Christmas Day</span>',
+	'12-31-2016' : '<span>New Year\'s Eve</span>'
+};
